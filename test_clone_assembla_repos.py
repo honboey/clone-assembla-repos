f22d43565975b9ace26e146053632174b340c384
Hon Boey <hon@dialogicstudios.com.au>
2023-07-18 06:09:32 +1000

    Fix test for make_json_of_spaces_repos
    Hardcode the test result rather than rely on it
    getting generated

--- test_clone_assembla_repos.py
+++ test_clone_assembla_repos.py
@@ -6,12 +6,6 @@ from clone_assembla_repos import (
 )
 
 # Test data
-with open("data/test-data/test_spaces.json", "r") as test_spaces_contents:
-    test_spaces = json.load(test_spaces_contents)
-
-with open("data/test-data/test_repo_list.json", "r") as test_repos_list_contents:
-    test_spaces_repos = json.load(test_repos_list_contents)
-
 test_list_of_spaces_ids = [
     "bHeZE2SX4r3PYIeJe5afGb",
     "a9LKRAvAWr4ir7eJe5cbLA",
@@ -23,9 +17,17 @@ test_list_of_repo_urls = [
     "git@git.assembla.com:abc-moma.git",
 ]
 
+with open(
+    "data/test-data/test_repo_list_result.json", "r"
+) as test_repo_list_result_json:
+    test_spaces_repos = json.load(test_repo_list_result_json)
+
 
 def test_make_list_of_spaces():
-    assert make_list_of_spaces(test_spaces) == test_list_of_spaces_ids
+    assert (
+        make_list_of_spaces("data/test-data/test_spaces.json")
+        == test_list_of_spaces_ids
+    )
 
 
 def test_make_json_of_spaces_repos():
