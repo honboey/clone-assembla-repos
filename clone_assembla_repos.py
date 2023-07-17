06788f056a7bfdc56ee97db041c734f1b3625773
Hon Boey <hon@dialogicstudios.com.au>
2023-07-17 17:14:20 +1000

    Parse list of space ids to json file of its repositories

--- clone_assembla_repos.py
+++ clone_assembla_repos.py
@@ -1,3 +1,11 @@
+import subprocess
+import json
+import os
+
+api_key = os.getenv("ASSEMBLA_KEY")
+api_secret = os.getenv("ASSEMBLA_SECRET")
+
+
 # JSON -> List
 # Given a JSON file of Space information, pull out the ID of each Space
 
@@ -9,9 +17,36 @@ def make_list_of_spaces(json):
     return list_of_spaces
 
 
-# List -> JSON 
-# Given a list of space ids, create a list of all the repos in those spaces
-# ["space-id-1", "space-id-2", "space-id-3"] -> [""]
-def make_json_of_spaces_repos(list):
-    ...
-    
+# Lst, Str -> JSON
+# Given a list of space ids and a file path, create a json file of all the repos in those spaces
+# ["space-id-1", "space-id-2", "space-id-3"], "file/path" -> JSON
+def make_json_of_spaces_repos(list, str):
+    # Create empty JSON file
+    repo_list_file = str
+    with open(repo_list_file, "w") as repo_list:
+        json.dump([], repo_list)
+
+    for space in list:
+        # Run terminal command to access Assembla API and request the repo information of the Space
+        get_repos = f"curl -H 'X-Api-Key: {api_key}' -H 'X-Api-Secret: {api_secret}' https://api.assembla.com/v1/spaces/{space}/repos.json"
+        output_raw = subprocess.run(
+            get_repos,
+            shell=True,
+            text=True,
+            capture_output=True,
+        ).stdout
+
+        # Convert raw output to JSON
+        output_json = json.loads(output_raw)
+
+        # Open and then add to the JSON file
+        with open(repo_list_file, "r") as repo_list:
+            existing_data = json.load(repo_list)
+        existing_data.append(output_json)
+        with open(repo_list_file, "w") as repo_list:
+            json.dump(existing_data, repo_list, indent=2)
+
+    with open(repo_list_file, "r") as repo_list:
+        repo_list_content = json.load(repo_list)
+    return repo_list_content
+
--- test_clone_assembla_repos.py
+++ test_clone_assembla_repos.py
@@ -8,8 +8,8 @@ from clone_assembla_repos import (
 with open("data/test-data/test_spaces.json", "r") as test_spaces_contents:
     test_spaces = json.load(test_spaces_contents)
 
-with open("data/test-data/test_spaces_repos.json", "r") as test_spaces_repos_contents:
-    test_spaces_repos = json.load(test_spaces_repos_contents)
+with open("data/test-data/test_repo_list.json", "r") as test_repos_list_contents:
+    test_spaces_repos = json.load(test_repos_list_contents)
 
 test_list_of_spaces_ids = [
         "bHeZE2SX4r3PYIeJe5afGb",
@@ -21,4 +21,4 @@ def test_make_list_of_spaces():
     assert make_list_of_spaces(test_spaces) == test_list_of_spaces_ids
 
 def test_make_json_of_spaces_repos():
-    assert make_json_of_spaces_repos(test_list_of_spaces_ids) == test_spaces_repos
+    assert make_json_of_spaces_repos(test_list_of_spaces_ids, "data/test-data/test_repo_list.json") == test_spaces_repos
